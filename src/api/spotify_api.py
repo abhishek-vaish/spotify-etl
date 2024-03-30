9a9a7721d55d30f812465afb050af0c2b8f963da
abhishek-vaish <abhishekv5055@gmail.com>
2024-03-30 11:21:22 +0530

    create spotify api class

--- src/api/spotify_api.py
+++ src/api/spotify_api.py
@@ -1,9 +1,31 @@
 import requests
 
+
 class SpotifyAPI:
-    __BASE_URL = "https://api.spotify.com/"
+    __BASE_URL = "https://api.spotify.com/v1/"
 
-    def __init__(self, access_token):
-        self.access_token = access_token
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
 
-    def get
+    def get_access_token(self):
+        uri = "https://accounts.spotify.com/api/token"
+        response = requests.post(uri, data={"grant_type": "client_credentials",
+                                            "client_id": self.client_id,
+                                            "client_secret": self.client_secret
+                                            }, headers={
+            "Content-Type": "application/x-www-form-urlencoded"
+        })
+        return response.json()['access_token']
+
+    def get_api_response(self, endpoints, access_token, key=None):
+        uri = self.__BASE_URL + f"{endpoints}" + f"/{key}" if key \
+            else self.__BASE_URL + f"{endpoints}"
+        try:
+            response = requests.get(uri, headers={"Authorization": f"Bearer {access_token}"})
+            if response.status_code == 200:
+                return response.json()
+            else:
+                raise requests.RequestException
+        except requests.ConnectionError:
+            raise requests.ConnectionError
---
+++ src/utility/spotify_config.py
@@ -0,0 +1,4 @@
+config = {
+    "albums": "4aawyAB9vmqN3uQ7FjRGTy",
+    "artists": "0TnOYISbd1XYRBk9myaseg"
+}
